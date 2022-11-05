3a710c037bc6ce2dcd6022ff17c5237f81610791
kok4444 <koba4444@gmail.com>
2022-11-05 15:20:59 +0200

    v1. Is it working?

--- src/get_reddit_subs.py
+++ src/get_reddit_subs.py
@@ -117,35 +117,7 @@ requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
 
 params = {'limit': 100}
-iterations = [{"cur_iter": 0, "SUBREDDIT": "wallstreetbets", "SUB_TYPE": "new", "params": {'limit': 100}},
-"""
-            {"cur_iter": 0, "SUBREDDIT": "wallstreetbets", "SUB_TYPE": "top", "params": {'limit': 100}},
-
-            {"cur_iter": 0, "SUBREDDIT": "wallstreetbets", "SUB_TYPE": "rising", "params": {'limit': 100}},
-            {"cur_iter": 0, "SUBREDDIT": "wallstreetbets", "SUB_TYPE": "hot", "params": {'limit': 100}},
- 
-              {"cur_iter": 0, "SUBREDDIT": "BBIG", "SUB_TYPE": "top", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "BBIG", "SUB_TYPE": "rising", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "BBIG", "SUB_TYPE": "hot", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "BBIG", "SUB_TYPE": "new", "params": {'limit': 100}},
-            {"cur_iter": 0, "SUBREDDIT": "Superstonk", "SUB_TYPE": "top", "params": {'limit': 100}},
-            {"cur_iter": 0, "SUBREDDIT": "Superstonk", "SUB_TYPE": "rising", "params": {'limit': 100}},
-            {"cur_iter": 0, "SUBREDDIT": "Superstonk", "SUB_TYPE": "hot", "params": {'limit': 100}},
-            {"cur_iter": 0, "SUBREDDIT": "Superstonk", "SUB_TYPE": "new", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "amcstock", "SUB_TYPE": "top", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "amcstock", "SUB_TYPE": "rising", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "amcstock", "SUB_TYPE": "hot", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "amcstock", "SUB_TYPE": "new", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "KOSSstock", "SUB_TYPE": "top", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "KOSSstock", "SUB_TYPE": "rising", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "KOSSstock", "SUB_TYPE": "hot", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "KOSSstock", "SUB_TYPE": "new", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "CLOV", "SUB_TYPE": "top", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "CLOV", "SUB_TYPE": "rising", "params": {'limit': 100}},
-              {"cur_iter": 0, "SUBREDDIT": "CLOV", "SUB_TYPE": "hot", "params": {'limit': 100}},
-"""
-              {"cur_iter": 0, "SUBREDDIT": "CLOV", "SUB_TYPE": "new", "params": {'limit': 100}}
-              ]
+iterations = [{"cur_iter": 0, "SUBREDDIT": "wallstreetbets", "SUB_TYPE": "new", "params": {'limit': 100}}]
 
 
 
