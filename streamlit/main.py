aefacea30d2041d06ea79ed15c50522744f490b1
kok4444 <koba4444@gmail.com>
2022-11-07 09:55:43 +0300

    Revert "v1. some changes in streamlit"
    This reverts commit 023b6a0b586390be12244b6c2950ebc55b8351f3.

--- streamlit/main.py
+++ streamlit/main.py
@@ -9,21 +9,18 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
-
-
-def wsb_sentiment():
-    data = pd.read_csv('output.csv')
-
-    col1, col2, col3 = st.columns([1,3])
-
-    col1.markdown(' # Wallstreetbet subreddit sentiment dynamics')
-    col1.markdown("""
-                Know what is sentiment on r/wallstreetbet - subreddit witn 13 mln readers
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    import streamlit as st
+    st.title('Wallstreetbet subreddit sentiment dinamics')
+    st.write("""
+                #My app
+                Hello *world!*/n
+                ##l;skjgirnigs
             """)
-    col1.metric(label="Sent value", val=3, delta=-1)
 
     chart_data = pd.DataFrame(
-        data,
+        np.random.randn(20, 3),
         columns=['a', 'b', 'c'])
 
     st.line_chart(chart_data)
@@ -104,7 +101,7 @@ def wsb_sentiment():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    wsb_sentiment()
+    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 #
