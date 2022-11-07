509f6cd861a943affd1818c5fddb800a9ef8a2ed
kok4444 <koba4444@gmail.com>
2022-11-07 13:53:59 +0300

    Revert "v.1.6"
    This reverts commit 46d03ff62d4545b45ae21938e25963eadbe66fc1.

--- streamlit/pages/p1.py
+++ streamlit/pages/p1.py
@@ -1,70 +1,25 @@
-from datetime import datetime
+# This is a sample Python script.
 
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+
+import pandas as pd
+import pydeck as pdk
 import streamlit as st
-from vega_datasets import data
-
-from utils import chart, db
-
-COMMENT_TEMPLATE_MD = """{} - {}
-> {}"""
 
 
-def space(num_lines=1):
-    """Adds empty lines to the Streamlit app."""
-    for _ in range(num_lines):
-        st.write("")
 
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
 
-st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting app")
+st.line_chart(chart_data)
 
-# Data visualisation part
+df = pd.DataFrame(
+    np.random.randn(100, 2) / [0.5, 0.5] + [55.5, 37.33],
+    columns=['lat', 'lon'])
+st.map(df)
 
-st.title("💬 Commenting app")
-
-source = data.stocks()
-all_symbols = source.symbol.unique()
-symbols = st.multiselect("Choose stocks to visualize", all_symbols, all_symbols[:3])
-
-space(1)
-
-source = source[source.symbol.isin(symbols)]
-chart = chart.get_chart(source)
-st.altair_chart(chart, use_container_width=True)
-
-space(2)
-
-# Comments part
-
-conn = db.connect()
-comments = db.collect(conn)
-
-with st.expander("💬 Open comments"):
-
-    # Show comments
-
-    st.write("**Comments:**")
-
-    for index, entry in enumerate(comments.itertuples()):
-        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
-
-        is_last = index == len(comments) - 1
-        is_new = "just_posted" in st.session_state and is_last
-        if is_new:
-            st.success("☝️ Your comment was successfully posted.")
-
-    space(2)
-
-    # Insert comment
-
-    st.write("**Add your own comment:**")
-    form = st.form("comment")
-    name = form.text_input("Name")
-    comment = form.text_area("Comment")
-    submit = form.form_submit_button("Add comment")
-
-    if submit:
-        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
-        if "just_posted" not in st.session_state:
-            st.session_state["just_posted"] = True
-        st.experimental_rerun()
+#======================================
